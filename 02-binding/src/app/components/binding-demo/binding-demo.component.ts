8626ae03b918569b0e897705fd516d645af6bf22
DarkT <cino.tessa@gmail.com>
2023-02-09 11:23:01 +0100

    Fondamentaux: binding
    Nous avons vu les différentes sortes de binding.
    Avec les [] pour lier des valeurs dans notre html à des propriétés de notre classe./du JS.
    Avec les () pour lier un évènement à une fonction de notre classe et avec les deux pour que la communication se fasse de la classe vers l'html et inversément.

---
+++ 02-binding/src/app/components/binding-demo/binding-demo.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-binding-demo',
+  templateUrl: './binding-demo.component.html',
+  styleUrls: ['./binding-demo.component.css']
+})
+export class BindingDemoComponent {
+  isNotAllowedToWrite = false;
+  isPremium = true;
+  username = "DarkT";
+
+  changeUsername(input: HTMLInputElement){
+    console.log(input);
+    this.username = input.value;
+    input.value = "";
+    
+  }
+}
